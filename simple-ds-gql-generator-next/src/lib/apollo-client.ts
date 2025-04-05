51eaf43d2674c3463a42dc81d58ff80bf7f582cf
YanaKirina <yanakirinayana@gmail.com>
2025-04-05 22:30:56 +0300

    fix

--- simple-ds-gql-generator-next/src/lib/apollo-client.ts
+++ simple-ds-gql-generator-next/src/lib/apollo-client.ts
@@ -1,15 +1,17 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 const httpLink = createHttpLink({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || '/graphql',
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || 'api/graphql',
   fetchOptions: {
     mode: 'cors',
     credentials: 'include',
   },
   headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type',
+    // 'Access-Control-Allow-Origin': '*',
+    // 'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+    // 'Access-Control-Allow-Headers': 'Content-Type',
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
   },
 });
 
