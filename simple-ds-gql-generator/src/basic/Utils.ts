58c538157b17e2759610f098c07e43f494c42316
YanaKirina <yanakirinayana@gmail.com>
2025-04-06 12:37:36 +0300

    del

--- simple-ds-gql-generator/src/basic/Utils.ts
+++
@@ -1,7 +0,0 @@
-export const extractParamName = (paramStr: string) => {
-    return paramStr.split("=")[0]
-}
-
-export const extractParamValue = (paramStr: string) => {
-    return paramStr.split("=")[1]
-}
